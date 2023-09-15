860e979fbac53beb5e16b01b25c6eb13028c7036
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-09-15 17:10:34 -0300

    adicionando services

--- app.js
+++ app.js
@@ -27,8 +27,10 @@ app.controller('HomeViewController', ['$scope', function ($scope) {
     $scope.appTitle = "Simple Expenses Tracker";
 }]);
 
-app.controller('ExpensesViewController', ['$scope', function ($scope) {
-    $scope.expenses = [
+app.factory('Expenses', function () {
+    let service = {};
+
+    service.entries = [
         { description: 'food', amount: 10, date: '2023-09-01' },
         { description: 'tickets', amount: 11, date: '2023-09-02' },
         { description: 'food', amount: 12, date: '2023-09-02' },
@@ -36,8 +38,14 @@ app.controller('ExpensesViewController', ['$scope', function ($scope) {
         { description: 'bills', amount: 13, date: '2023-09-03' },
         { description: 'food', amount: 15, date: '2023-09-04' }
     ];
-}]);
-app.controller('ExpenseViewController', ['$scope', '$routeParams', function ($scope, $routeParams) {
-    $scope.someText = 'The world is round. ID = ' + $routeParams.id;
 
+    return service;
+});
+
+app.controller('ExpensesViewController', ['$scope', 'Expenses', function ($scope, Expenses) {
+    $scope.expenses = Expenses.entries;
+}]);
+
+app.controller('ExpenseViewController', ['$scope', '$routeParams', 'Expenses', function ($scope, $routeParams, Expenses) {
+    $scope.someText = 'The world is round. ID = ' + $routeParams.id + '. The first entry is: ' + Expenses.entries[0].description;
 }]);
